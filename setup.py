499eb3e69256720e3a1d2e9e9bb14b304743c83c
Rayhan Hussain <135307421+rayhanhcse@users.noreply.github.com>
2025-10-18 15:37:00 +0600

    requiremnets added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa",
+    version="0.0.0",
+    author="Rayhan",
+    author_email="rayhanhcse@gmail.com",
+    packages=find_packages()
+)
